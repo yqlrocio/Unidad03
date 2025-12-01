35b652c79129d2c419a476af7e2f03b7d4543e2c
qr.ye <qr.ye@106-27.info2>
2025-12-01 10:13:01 +0100

    sin terminar

---
+++ Unidad03/src/boletin01/Ejercicio01.java
@@ -0,0 +1,33 @@
+package boletin01;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		
+		// Crear Scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// Crear número aleatorio
+		Random rand = new Random(); 
+		
+		// Inicializar lista
+
+		 int[] tabla = new int[10];
+
+	     for (int i = 0; i < tabla.length; i++) {
+		 tabla[i] = rand.nextInt(100) + 1; // valores entre 1 y 100
+		  }
+
+		 // Mostrar el contenido de la tabla
+		 for (int num : tabla) {
+		  System.out.print(num + " ");
+		}
+		 
+		 // Cerrar Scanner 
+		 reader.close(); 
+		 
+	}
+}
